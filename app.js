f831d5e0c3e6666b58ba01d2ac718b1324600b79
branxrams <braraos21@gmail.com>
2023-06-07 13:59:40 -0500

    fix: repair database conection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./src/routes/index');
+var apiIndexRouter = require('./src/routes/api/apiIndex');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+// app.use('/api', apiIndexRouter);
 
 //catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ src/routes/api/apiIndex.js
@@ -0,0 +1,12 @@
+var express = require('express');
+const apiMainController = require('../../controllers/api/apiMainController');
+var router = express.Router();
+
+
+// router.get('/colegios', apiMainController);
+// router.get('/pedidos', apiMainController);
+// router.get('/colegios', apiMainController);
+// router.get('/colegios', apiMainController);
+
+
+return router;
