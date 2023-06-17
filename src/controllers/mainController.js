15a09758a3255da4eb13c75ec60cf3c49c6cfd7c
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-06-16 19:25:11 -0500

    Delete: Delete title variable on signin page

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,27 +1,32 @@
 const { validationResult } = require("express-validator");
 const db = require("../models");
 const { Op } = require("sequelize");
-const hash = require('../helpers/hash');
+const hash = require("../helpers/hash");
 const Empleados = db.Empleado;
 
 const mainController = {
   index: (req, res) => {
-    res.render('index', {title: 'Inicio'});
+    res.render("index", { title: "Inicio" });
   },
-  signinPage: (req, res)=> {
-    res.render('signin', {title: 'Registro'})
+  signinPage: (req, res) => {
+    res.render("signin");
   },
   loginPage: (req, res) => {
-    res.render('login', {title: 'Iniciar Sesión'})
+    res.render("login", { title: "Iniciar Sesión" });
   },
-  signin: (req, res) => { 
+  signin: (req, res) => {
     let errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      res.render('signin', {title: 'Registro', errors: errors.mapped(), oldData: req.body});
-    }else{
-      hash.generateHashAsync(req.body.password)
-        .then(hash => {
+      res.render("signin", {
+        title: "Registro",
+        errors: errors.mapped(),
+        oldData: req.body,
+      });
+    } else {
+      hash
+        .generateHashAsync(req.body.password)
+        .then((hash) => {
           let data = {
             nombre: req.body.nombre,
             usuario: req.body.celular,
@@ -29,66 +34,70 @@ const mainController = {
             rol: req.body.cargo,
           };
           Empleados.create(data)
-            .then(status => {
-              res.render('login', {title: 'Iniciar Sesión', status: status});
+            .then((status) => {
+              res.render("login", { title: "Iniciar Sesión", status: status });
             })
-            .catch(error => console.log(error));
+            .catch((error) => console.log(error));
         })
-        .catch(error => console.log(error))
+        .catch((error) => console.log(error));
     }
   },
   login: (req, res) => {
-    let errors =  validationResult(req);
+    let errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      res.render('login', {title: "Iniciar Sesión", errors: errors.mapped(), oldData: req.body.celular});
-    }else{
-      const { celular, password} = req.body;
+      res.render("login", {
+        title: "Iniciar Sesión",
+        errors: errors.mapped(),
+        oldData: req.body.celular,
+      });
+    } else {
+      const { celular, password } = req.body;
       Empleados.findAll({
         where: {
-          usuario: {[Op.like]: celular }
-        }
+          usuario: { [Op.like]: celular },
+        },
       })
-      .then( user => {
-        if (user.length === 0) {
-          return res.render('login', {
-            title: 'Iniciar Sesión',
-            errors: {
-              badUser:{
-                msg: 'Usuario no econtrado'
-              }
-            }
-          });
-        }else{
-          const data = user[0];
-          hash.comparePasswordAsync(password, data.contrasena)
-            .then(match => {
-              console.log(data.contrasena);
-              if(!match){
-              return res.render('login', {
-                  title: 'Iniciar Sesión',
-                  errors: {
-                    badPass:{
-                      msg: 'Contraseña Invalida'
-                    }
-                  }
-                });
-              }
-              req.session.loggedUSer = data.usuario;
-              res.render('opcPrincipales', { title: "Opciones Principales" });
-            })
-            .catch(error => {
-              console.log(error);
-            })
-        }
-        
-      })
-      .catch(error => console.log(error));
+        .then((user) => {
+          if (user.length === 0) {
+            return res.render("login", {
+              title: "Iniciar Sesión",
+              errors: {
+                badUser: {
+                  msg: "Usuario no econtrado",
+                },
+              },
+            });
+          } else {
+            const data = user[0];
+            hash
+              .comparePasswordAsync(password, data.contrasena)
+              .then((match) => {
+                console.log(data.contrasena);
+                if (!match) {
+                  return res.render("login", {
+                    title: "Iniciar Sesión",
+                    errors: {
+                      badPass: {
+                        msg: "Contraseña Invalida",
+                      },
+                    },
+                  });
+                }
+                req.session.loggedUSer = data.usuario;
+                res.render("opcPrincipales", { title: "Opciones Principales" });
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          }
+        })
+        .catch((error) => console.log(error));
     }
   },
   opcPrincipales: (req, res) => {
     res.render("opcPrincipales", { title: "Opciones Principales" });
-  }
+  },
 };
 
 module.exports = mainController;
