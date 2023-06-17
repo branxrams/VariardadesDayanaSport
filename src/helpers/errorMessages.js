2794b2cd372750c5c963ddfa4972884a715c69ef
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-06-16 20:06:02 -0500

    Feat: Created error messages dictionary

---
+++ src/helpers/errorMessages.js
@@ -0,0 +1,7 @@
+const ERROR_MESSAGES = Object.freeze({
+  NOT_FOUND_USER: "Usuario no encontrado",
+  BAD_PASSWORD: "Contraseña invalida",
+  BAD_LOGIN: "Debes Iniciar sesión primero",
+});
+
+module.exports = ERROR_MESSAGES;
